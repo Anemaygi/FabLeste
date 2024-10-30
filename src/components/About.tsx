8986c79127f0fcf25b7239023a4557d9df6509c5
Anemaygi <gianemayumisan@gmail.com>
2024-10-30 18:15:54 -0300

    style: fix padding

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,8 +6,8 @@ export const About = () => {
       id="about"
       className="container py-24 sm:py-32"
     >
-      <div className="bg-muted/50 border rounded-lg p-12 ">
-        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+      <div className="bg-muted/50 border rounded-lg p-2 lg:p-12 ">
+        <div className="px-6 flex items-center flex-col-reverse md:flex-row gap-8 md:gap-12">
           <img
             src={polaroid1}
             alt=""
@@ -15,7 +15,7 @@ export const About = () => {
           />
           <div className="bg-green-0 flex flex-col justify-between items-center">
             <div className="">
-              <h2 className="text-3xl md:text-4xl font-bold">
+              <h2 className="text-3xl md:text-4xl font-bold pt-4">
                 <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
                   Conheça{" "}
                 </span>
